902fe5e1b5d49a404517a2bf6efbfcf70c3700b7
kareena <angewills387@gmail.com>
2023-12-09 23:40:02 +0545

    addefile

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resorts | Admin Dashboard</title>
+    <link rel="stylesheet" href="../css/admin.css">
+
+</head>
+
+<body>
+    <!-- Menu Section starts -->
+    <div class="menu">
+        <div class="wrapper">
+            Menu Goes Here
+        </div>
+    </div>
+    <!--  Menu Section Ends -->
+
+    <!--  Main Content Section Starts -->
+    <div class="main-content">
+        Main Content Goes Here
+    </div>
+    <!--  Main Content Section Ends -->
+
+    <!--  Footer Content Section Starts -->
+    <div class="footer">
+        Footer Goes Here
+    </div>
+    <!--  Footer Content Section Ends -->
+
+</body>
+
+</html>
